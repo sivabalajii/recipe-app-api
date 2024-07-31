4c8d7b5178f3ccd9bbf3b906a7ca9f87926ff522
sivabalajee <sivabalaji.dharman@insure-ai.io>
2024-07-31 15:44:33 +0530

    Added Admin-Updated

--- app/core/models.py
+++ app/core/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
